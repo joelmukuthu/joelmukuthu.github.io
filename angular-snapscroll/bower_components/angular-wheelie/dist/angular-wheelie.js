9ea44422f54013acef3105b85863d67607cc3870
Joel Mukuthu <jmu@one.com>
2016-11-05 18:08:43 +0100

    Update angular-wheelie to 2.0.0

--- angular-snapscroll/bower_components/angular-wheelie/dist/angular-wheelie.js
+++ angular-snapscroll/bower_components/angular-wheelie/dist/angular-wheelie.js
@@ -1,9 +1,9 @@
 /**
  * angular-wheelie
- * Version: 1.1.0
- * (c) 2014-2016 Joel Mukuthu
+ * Version: 2.0.0
+ * (c) 2016 Joel Mukuthu
  * MIT License
- * Built on: 16-07-2016 00:30:42 GMT+0200
+ * Built on: 05-11-2016 17:55:10 GMT+0100
  **/
 
 angular.module('wheelie', []);
@@ -11,48 +11,59 @@ angular.module('wheelie', []);
 angular
 .module('wheelie')
 .factory('wheelie', [function () {
+    var isDefined = angular.isDefined;
+    var isFunction = angular.isFunction;
+
     return {
         bind: function (element, callbacks) {
             callbacks = callbacks || {};
-            if (angular.isDefined(callbacks.up) && !angular.isFunction(callbacks.up)) {
+
+            if (isDefined(callbacks.up) && !isFunction(callbacks.up)) {
                 throw new Error('The \'up\' callback must be a function');
             }
-            if (angular.isDefined(callbacks.down) && !angular.isFunction(callbacks.down)) {
+            if (isDefined(callbacks.down) && !isFunction(callbacks.down)) {
                 throw new Error('The \'down\' callback must be a function');
             }
-            if (!angular.isDefined(callbacks.up) && !angular.isDefined(callbacks.down)) {
-                throw new Error('At least one callback (\'up\' or \'down\') must be provided');
+            if (isDefined(callbacks.left) && !isFunction(callbacks.left)) {
+                throw new Error('The \'left\' callback must be a function');
+            }
+            if (isDefined(callbacks.right) && !isFunction(callbacks.right)) {
+                throw new Error('The \'right\' callback must be a function');
+            }
+            if (!isDefined(callbacks.up) &&
+                !isDefined(callbacks.down) &&
+                !isDefined(callbacks.left) &&
+                !isDefined(callbacks.right)) {
+                throw new Error(
+                    'At least one callback (\'up\', \'down\', \'left\' or \'right\') must be provided'
+                );
             }
 
             function bindWheel(e) {
                 if (e.originalEvent) {
                     e = e.originalEvent;
                 }
-                var delta;
-                delta = Math.max(-1, Math.min(1, (e.wheelDelta || -(e.deltaY || e.detail))));
-                if (isNaN(delta) || delta === 0) {
-                    return;
+
+                if (e.deltaX) {
+                    e.deltaX < 0 && callbacks.left && callbacks.left(e);
+                    e.deltaX > 0 && callbacks.right && callbacks.right(e);
                 }
-                if (delta > 0) {
-                    if (callbacks.up) {
-                        callbacks.up(e);
-                    }
-                } else {
-                    if (callbacks.down) {
-                        callbacks.down(e);
-                    }
+
+                if (e.deltaY) {
+                    e.deltaY < 0 && callbacks.up && callbacks.up(e);
+                    e.deltaY > 0 && callbacks.down && callbacks.down(e);
                 }
             }
 
             element.data('___wheelie_bindWheel___', bindWheel);
-            element.on('wheel mousewheel onmousewheel', bindWheel);
+            element.on('wheel', bindWheel);
         },
 
         unbind: function (element) {
             var bindWheel = element.data('___wheelie_bindWheel___');
-            if (angular.isFunction(bindWheel)) {
+            if (isFunction(bindWheel)) {
                 element.data('___wheelie_bindWheel___', null);
-                element.off('wheel mousewheel onmousewheel', bindWheel);
+                element.off('wheel', bindWheel);
             }
         }
     };
